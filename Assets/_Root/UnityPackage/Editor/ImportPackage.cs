96f9b8fcdbe9dcf976f4662f44dc837e93f03218
yenmoc <phongsoyenmoc.diep@gmail.com>
2021-12-24 11:25:55 +0700

    *bug fix path import package

--- Assets/_Root/UnityPackage/Editor/ImportPackage.cs
+++ Assets/_Root/UnityPackage/Editor/ImportPackage.cs
@@ -8,7 +8,7 @@ public static class ImportPackage
 #if !SNORLAX_LEVEL_EDITOR
     private const string LEVEL_EDITOR_PACKAGE_PATH = "Assets/_Root/UnityPackage/level-editor.unitypackage";
     private const string PATH_INSTALL = "Assets/Plugins/Demigiant/DOTween";
-    private const string PACKAGE_PATH = "Packages/com.snorlax.level-editor/UnityPackage/level-editor.unitypackage";
+    private const string PACKAGE_PATH = "Packages/com.snorlax.level-editor/level-editor.unitypackage";
 
     [MenuItem("Package/Import LevelEditor")]
     [InitializeOnLoadMethod]
